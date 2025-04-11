5cf58f7fe4198db09c8b7e74112af7d52fd9b804
Fernando Santos <sombarepam@gmail.com>
2025-04-11 11:49:58 -0300

    Exercício 7 (Desafio): Moedas para 12 reais

---
+++ while7.js
@@ -0,0 +1,12 @@
+let f = 1;
+let total1 = 0
+let total2 = 0
+
+while (total1 < 12) {
+    total1 = total1 + f;
+    f++;
+    console.log(`total: ${total1}`)
+    total2 = total2 + f;
+    f++;
+    console.log(`total: ${total2}`)
+}
